cc31f08c0a0cb99a5d7ec118a7ab2f3b9b19a55e
jayantt23 <jayant.sharma@iiitb.ac.in>
2024-06-11 12:54:33 +0530

    Write logic to get human choice

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 let getComputerChoice = function () {
   let randomNumber = Math.floor(Math.random() * 100) % 3;
+
   if (randomNumber == 0) return "rock";
   else if (randomNumber == 1) return "paper";
   else if (randomNumber == 2) return "scissors";
+};
+
+let getHumanChoice = function () {
+  let choice = prompt("Enter your choice:");
+  choice = choice.toLowerCase();
+
+  if (choice == "rock" || choice == "paper" || choice == "scissors")
+    return choice;
+  else {
+    console.log(`Invalid choice. Please enter something from the following:
+- Rock
+- Paper
+- Scissors`);
+    return getHumanChoice();
+  }
 };
